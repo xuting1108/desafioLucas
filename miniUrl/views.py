e4542b71a7af3e8d5afb38aed338aaca250e3ac2
Xuting <xuting1108@hotmail.com>
2019-01-28 21:43:00 -0200

    2

--- miniUrl/views.py
+++ miniUrl/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Link
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+class LinkCriado(CreateView):
+    model = Link
+    campos = ['url']
