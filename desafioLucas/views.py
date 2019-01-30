a88065580e7405920b27582a6a61281bcf4562f1
Xuting <xuting1108@hotmail.com>
2019-01-30 08:34:37 -0200

    5

--- desafioLucas/views.py
+++ desafioLucas/views.py
@@ -1,7 +1,29 @@
 from desafioLucas.models import Link
 from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views.generic.base import RedirectView
+from django.shortcuts import redirect
+
 
 # Create your views here.
 class LinkCriado(CreateView):
     model = Link
     campos = ['url']
+
+    def form_valid(self, form):
+        prev = Link.objects.filter(url=form.instance.url)
+        if prev:
+            return redirect("link_show", pk=prev[0].pk)
+        return super(LinkCriado, self).form_valid(form)
+
+
+class LinkShow(DetailView):
+    model = Link
+
+
+class RedirectToLongURL(RedirectView):
+    permanent = False
+
+    def get_redirect_url(self, *args, **kwargs):
+        short_url = kwargs["short_url"]
+        return Link.expand(short_url)
